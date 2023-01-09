d7add0c5c39628254837ec2371eb79cf0c051f6c
nik8067 <niks15543@gmail.com>
2023-01-10 00:11:38 +0200

    Com 1

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// const totalPrice = 200.45;
+// console.log("Цена: ", totalPrice);
+// console.log('5');
+
+// const type = typeof true;
+// console.log(type);
+
+// console.log('До');
+// alert('dfdfdf');
+// console.log('После');
+
+// const SchouldRenew = confirm('Хочете продовжити підписку?');
+// console.log(SchouldRenew);
+// let quantity = prompt('Кількість товару');
+// quantity = Number(quantity);
+// console.log(quantity);
+// console.log(typeof quantity);
+
+let elementWidth = '50px';
+elementWidth = Number.parseInt(elementWidth);
+console.log('elementWidth:' , elementWidth);
+
+let elementHeight = '200.74px';
+elementHeight = Number.parseFloat(elementHeight);
+console.log('elementHeight:' , elementHeight);
